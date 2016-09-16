4188d0a488cfb2a74aee89142cf29ec97b37e6b2
Mir4ge <dreamscometru3@gmail.com>
2016-09-16 19:54:48 +0200

    - update sth

--- Magisterka/Controllers/HomeController.cs
+++ Magisterka/Controllers/HomeController.cs
@@ -10,7 +10,7 @@ namespace Magisterka.Controllers
     {
         public ActionResult Index()
         {
-            ViewBag.Message = "Modify this template to jump-start your ASP.NET MVC application.";
+            ViewBag.Message = "chuj";
 
             return View();
         }
