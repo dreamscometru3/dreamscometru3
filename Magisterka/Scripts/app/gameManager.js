9aff642f931dcc5103a375ed1b70b1ea2d9ec93b
mackor <mackorssj@gmail.com>
2016-09-17 13:49:09 +0200

    yyy

---
+++ Magisterka/Scripts/app/gameManager.js
@@ -0,0 +1,29 @@
+﻿define(["/Scripts/app/flipGame.js", "/Scripts/app/logicGame.js"], function (flipGame, logicGame) {
+
+    return {
+        init: function (isTest, game) {
+            var scope = this;
+            if (isTest) {
+                console.log('game Manager Test');
+                var randomGame = Math.round(Math.random());
+                this.initGame(randomGame, isTest);
+            } else {
+                $('.startGame').bind('click', function () {
+                    $('.content').hide();
+                    scope.initGame(game, isTest);
+                });
+            }
+        },
+
+        initGame: function (game, isTest) {
+            if (game == 0) {
+                flipGame.init(isTest);
+                $('.flip').show();
+                return;
+            }
+
+            $('.logic').show();
+            logicGame.init(isTest);
+        }
+    }
+});
